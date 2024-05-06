90e71466df93228ad376b2496b27044643491b7a
Nevi32 <150677438+Nevi32@users.noreply.github.com>
2024-05-06 23:25:25 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
+
 <?php
 $databaseConfig = [
-    'host' => 'localhost',
-    'dbname' => 'Duka',
+    'host' => 'fx0.h.filess.io',
+    'dbname' => 'Memo_nearerturn',
+    'port' => 3307,
     'user' => 'nevill',
-    'password' => 'nevill'
+    'password' => '7683Nev!//'
 ];
 ?>
 
